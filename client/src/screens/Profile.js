8a3f1e375945d6ce16109642a0485586c58b615a
Tusharshrivastav <ts3657@gmail.com>
2021-04-15 12:04:50 +0530

    add profile page in react js

--- client/src/screens/Profile.js
+++ client/src/screens/Profile.js
@@ -1,7 +1,97 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div style={{ maxWidth: "550px", margin: "0px auto" }}>
+      <div
+        style={{
+          margin: "18px 0px",
+          borderBottom: "1px solid grey",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <div>
+            <img
+              style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+              src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+            />
+          </div>
+          <div>
+            <h4></h4>
+            <h5></h5>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "108%",
+              }}
+            >
+              <h6> posts</h6>
+              <h6>followers</h6>
+              <h6> following</h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="file-field input-field" style={{ margin: "10px" }}>
+          <div className="btn #64b5f6 blue darken-1">
+            <span>Update pic</span>
+            <input type="file" />
+          </div>
+          <div className="file-path-wrapper">
+            <input className="file-path validate" type="text" />
+          </div>
+        </div>
+      </div>
+      <div className="gallery">
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          alt="gellery"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          alt="gellery"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          alt="gellery"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          alt="gellery"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          alt="gellery"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          alt="gellery"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          alt="gellery"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          alt="gellery"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
