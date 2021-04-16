7cb550c8fef2dda2e5f156513f510750d5e31d64
Tusharshrivastav <ts3657@gmail.com>
2021-04-16 09:53:23 +0530

    add protected navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,39 @@
 import React from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../App";
 
 const Navbar = () => {
+  const { state, dispatch } = useContext(UserContext);
+  const renderList = () => {
+    if (state) {
+      return [
+        <li>
+          <Link to="profile">Profile</Link>
+        </li>,
+        <li>
+          <Link to="create">Create-Post</Link>
+        </li>,
+      ];
+    } else {
+      return [
+        <li>
+          <Link to="login">Login</Link>
+        </li>,
+        <li>
+          <Link to="register">Register</Link>
+        </li>,
+      ];
+    }
+  };
   return (
     <nav>
       <div className="nav-wrapper white" style={{ color: "black" }}>
-        <Link to="/" className="brand-logo left logo">
+        <Link to={state ? "/" : "/login"} className="brand-logo left logo">
           Instagram
         </Link>
         <ul id="nav-mobile" className="right">
-          <li>
-            <Link to="login">Login</Link>
-          </li>
-          <li>
-            <Link to="register">Register</Link>
-          </li>
-          <li>
-            <Link to="profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="create">Create-Post</Link>
-          </li>
+          {renderList()}
         </ul>
       </div>
     </nav>
