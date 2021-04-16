9578dfca18b8c661ea279e72765338200e907ba6
Tusharshrivastav <ts3657@gmail.com>
2021-04-16 19:29:58 +0530

    add like unlike feature in react with api

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { useState } from "react";
+import { UserContext } from "../App";
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const { state, dispatch } = useContext(UserContext);
 
   useEffect(() => {
     fetch("/allpost", {
@@ -13,9 +15,59 @@ const Home = () => {
     })
       .then((res) => res.json())
       .then((result) => {
+        console.log(result);
         setData(result.posts);
       });
   }, []);
+  const likepost = (id) => {
+    fetch("/like", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const unlikepost = (id) => {
+    fetch("/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      });
+  };
   return (
     <div className="home">
       {data.map((item) => {
@@ -25,11 +77,34 @@ const Home = () => {
             <div className="card-image">
               <img src={item.photo} alt="post" />
             </div>
-            <div className="card-content">
-              <i className="material-icons " style={{ color: "red" }}>
+            <div className="card-content ">
+              <i
+                className="material-icons "
+                style={{ color: "red", paddingRight: "5px," }}
+              >
                 favorite
               </i>
-              <h4>{item.title}</h4>
+              {item.likes.includes(state._id) ? (
+                <i
+                  className="material-icons"
+                  onClick={() => {
+                    unlikepost(item._id);
+                  }}
+                >
+                  thumb_down
+                </i>
+              ) : (
+                <i
+                  className="material-icons"
+                  onClick={() => {
+                    likepost(item._id);
+                  }}
+                >
+                  thumb_up
+                </i>
+              )}
+              <h6>{item.likes.length} likes</h6>
+              <h6>{item.title}</h6>
               <p>{item.body}</p>
               <input type="text" placeholder="add a comment" />
             </div>
