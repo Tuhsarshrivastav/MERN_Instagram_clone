95e55dde942532b3fc2c8fc9f1a2883966ba1871
Tusharshrivastav <ts3657@gmail.com>
2021-04-17 07:36:35 +0530

    create comment api & connent with react js

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -15,7 +15,6 @@ const Home = () => {
     })
       .then((res) => res.json())
       .then((result) => {
-        console.log(result);
         setData(result.posts);
       });
   }, []);
@@ -68,6 +67,35 @@ const Home = () => {
         setData(newData);
       });
   };
+
+  const makeComment = (text, postId) => {
+    fetch("/comment", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId,
+        text,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="home">
       {data.map((item) => {
@@ -106,7 +134,24 @@ const Home = () => {
               <h6>{item.likes.length} likes</h6>
               <h6>{item.title}</h6>
               <p>{item.body}</p>
-              <input type="text" placeholder="add a comment" />
+              {item.comments.map((record) => {
+                return (
+                  <h6 key={record._id}>
+                    <span style={{ fontWeight: "500" }}>
+                      {record.postedBy.name}
+                    </span>{" "}
+                    {record.text}
+                  </h6>
+                );
+              })}
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  makeComment(e.target[0].value, item._id);
+                }}
+              >
+                <input type="text" placeholder="add a comment" />
+              </form>
             </div>
           </div>
         );
--- models/post.js
+++ models/post.js
@@ -16,7 +16,13 @@ const postSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    likes:[{type:ObjectId,ref:"User"}],
+    likes: [{ type: ObjectId, ref: "User" }],
+    comments: [
+      {
+        text: String,
+        postedBy: { type: ObjectId, ref: "User" },
+      },
+    ],
     postedBy: {
       type: ObjectId,
       ref: "User",
